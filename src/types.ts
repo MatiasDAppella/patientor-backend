325c2b0cd4c7144e08036d951a8015513aa9deb9
MatiasDAppella <appellamatias@gmail.com>
2024-01-08 14:24:12 -0300

    chore: Entry type defined

--- src/types.ts
+++ src/types.ts
@@ -10,9 +10,51 @@ export enum Gender {
   Other = "Other",
 }
 
-export interface Entry {
-
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
 }
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string;
@@ -25,3 +67,9 @@ export interface Patient {
 }
 
 export type NonSensitivePatients = Omit<Patient, "ssn" | "entries">;
+
+// Special Omit definition for unions.
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+
+// Define Entry whitout 'id' property.
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
